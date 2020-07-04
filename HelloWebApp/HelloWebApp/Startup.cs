88d70704912cad75ff4632a3100aeaeaf67b64dd
Amit Ravi <hiamit.ravi@gmail.com>
2020-07-04 16:00:14 +0100

    updated code

--- HelloWebApp/HelloWebApp/Startup.cs
+++ HelloWebApp/HelloWebApp/Startup.cs
@@ -32,7 +32,7 @@ namespace HelloWebApp
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello Azure!!!!");
                 });
             });
         }
